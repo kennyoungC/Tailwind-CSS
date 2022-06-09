733a4b313b7222ca818a1e9215f8593df7ba03f9
Kenneth <obikennethe913@gmail.com>
2022-06-10 00:44:18 +0300

    all in one section in progress

---
+++ src/Components/AllinOne.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const AllinOne = () => {
+  return (
+    <div>
+      <div>
+        <h2>All-In-One Platform</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+          veniam adipisci fugit id nihil voluptatum.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default AllinOne
