71e4a103a79d5a8653e9fbda37312a0867f4963f
Kenneth <obikennethe913@gmail.com>
2022-06-10 00:44:53 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import About from "./Components/About"
+import AllinOne from "./Components/AllinOne"
+import Hero from "./Components/Hero"
+import Navbar from "./Components/Navbar"
+import Support from "./Components/Support"
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Support />
+      <AllinOne />
+    </>
+  )
+}
+
+export default App
